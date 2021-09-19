961bf222008a30c098f80508b7eb98037738660b
Ubuntu <ubuntu@ip-172-31-46-199.us-west-2.compute.internal>
2021-09-19 06:23:11 +0000

    final

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hi, My name is Rajeev, this is green deployment of the application version 1.0 "
+    return "Hi, My name is Rajeev, this is Blue deployment of the application version 2.0 "
 app.run(host="0.0.0.0", port=8080, debug=True)
