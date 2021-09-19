9f7ccb31090ee3eab4bec2d82d34dbae7a7d6c5e
Ubuntu <ubuntu@ip-172-31-33-109.us-west-2.compute.internal>
2021-09-19 05:27:29 +0000

    adding fist commit

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hi, This is Prakash Rajpurohit's Udacity final project. Blue Deployment Version 2.0 "
+    return "Hi, My name is Rajeev, this is green deployment of the application version 1.0 "
 app.run(host="0.0.0.0", port=8080, debug=True)
