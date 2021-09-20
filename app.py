45a825356a55b45e094150060d66fc2e40f1ddf5
Rajeevk1391 <85566928+Rajeevk1391@users.noreply.github.com>
2021-09-20 18:00:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hi, My name is Rajeev, this is Blue deployment of the application version 2.0 "
+    return "Hi, this is my copuston project, this is Blue deployment of the application version 2.0 "
 app.run(host="0.0.0.0", port=8080, debug=True)
