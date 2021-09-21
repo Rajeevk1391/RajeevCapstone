1fde1e56cb21dfb9375ea486fa99789d4df1babb
Ubuntu <ubuntu@ip-172-31-35-21.us-west-2.compute.internal>
2021-09-21 05:07:26 +0000

    comit

--- application.py
+++ application.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hi, this is copestone project under rollover stretegies , this is version 2.0  "
+    return "Hi, this is capstone project under rollover stretegies , this is version 2.0  "
 app.run(host="0.0.0.0", port=8080, debug=True)
